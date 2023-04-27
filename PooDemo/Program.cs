12619cf2561ee552073a2cdf9a6bba97aeffd22b
Miguel Teheran <miguelteheran@gmail.com>
2023-04-26 22:13:26 -0500

    agregando codigo base

---
+++ PooDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Apuntador apuntadorPlatzi = new Apuntador();
+Apuntador apuntadorPlatzi2 = new Apuntador();
+
+apuntadorPlatzi.Color = "Negro";
+
+apuntadorPlatzi2.Color = "Blanco";
+
+class Apuntador
+{
+    public string Color;
+    public double Largo;
+    public short NumeroDeBotones;
+    public bool TieneStickers;
+}
